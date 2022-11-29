c8239e5cb7117802ed7a131753da1d57ad68af59
RubenCodeTech <pzgz.ruben@gmail.com>
2022-11-30 00:21:21 +0100

    finished

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-let inputEmail = document.querySelectorAll('.form-control:first-of-type>input:first-of-type');
-let spanEmail = document.querySelectorAll('.form-control:first-of-type>label:first-of-type>span');
+let inputEmail = document.querySelector('.form-control:first-child>input');
+let spanEmail = document.querySelectorAll('.form-control:first-child>label>span');
 let spanPassword = document.querySelectorAll('.form-control:nth-child(2)>label>span');
-let inputPassword = document.querySelectorAll('.form-control:nth-child(2)>input')
+let inputPassword = document.querySelector('.form-control:nth-child(2)>input');
 
+let body = document.querySelector('body').addEventListener('focusout', () => {
+    spanEmail.forEach(element => {
+        element.classList.remove('spanY', 'colorAdd')
+        inputEmail.classList.remove('colorAdd')
+        if (inputEmail.value !== "") {
+            element.classList.add('spanY', 'colorAdd')
+            inputEmail.classList.add('colorAdd')
+        }
+    });
+    spanPassword.forEach(element => {
+        element.classList.remove('spanY', 'colorAdd')
+        inputPassword.classList.remove('colorAdd')
+        if (inputPassword.value !== "") {
+            element.classList.add('spanY', 'colorAdd')
+            inputPassword.classList.add('colorAdd')
+        }
+    });
+})
 
 for (let i = 0; i < spanEmail.length; i++) {
-    inputEmail.forEach(element => {
-        element.addEventListener('click', () => {
-            spanEmail[i].classList.add('spanY')
-        })
+
+    inputEmail.addEventListener('click', () => {
+        spanEmail[i].classList.add('spanY', 'colorAdd')
+        inputEmail.classList.add('colorAdd')
     });
 }
-
 for (let i = 0; i < spanPassword.length; i++) {
-    inputPassword.forEach(element => {
-        element.addEventListener('click', () => {
-            spanPassword[i].classList.add('spanY')
-            
-        })
+
+    inputPassword.addEventListener('click', () => {
+        spanPassword[i].classList.add('spanY', 'colorAdd')
+        inputPassword.classList.add('colorAdd')
     });
 }
-
-
-
-document.addEventListener('click', function(event) {
-    var isClickInsideElement = inputEmail.contains(event.target);
-    if (!isClickInsideElement) {
-        //Do something click is outside specified element
-    }
-});
